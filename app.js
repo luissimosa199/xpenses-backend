9b490d9ebe3add834ad0fec28630da5c9216309a
Luis Simosa <simosa37@gmail.com>
2022-09-29 13:26:54 -0300

    fix cors error with cors middleware

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const bills = require('./routes/bills')
 const port = 5000
 
+// cors
+app.use(cors())
 // parse form data
 app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
 // base route
 app.use('/api/v1/bills', bills)
-
-// routes zxx
-app.get('/home', (req, res) => {
-    res.send('Home')
-})
 
 app.listen(port, console.log(`Server Listening on port ${port}`))
